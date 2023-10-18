59ffa9565089754ab346a85721059a7b809f5c22
MoSalah07 <notway10@yahoo.com>
2023-10-18 13:32:22 +0300

    fixed-mode

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -1,28 +1,36 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LinksPages from "./LinksPages";
 import Logo from "./Logo";
 // ReactIcons
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu } from "react-icons/ai";
 // hook Dark Mode
 import useThemeSwitcher from "../../hooks/useThemeSwitcher";
 
 function Nav() {
+  const { mode, setMode } = useThemeSwitcher();
+  const [isRanderModeSvg, setIsRanderModeSvg] = useState(false);
 
-  const [mode, setMode] = useThemeSwitcher();
+  useEffect(() => {
+    setIsRanderModeSvg(true);
+  }, []);
 
   return (
-    <nav className="h-[10vh] shadow-lg flex items-center">
-      {/* Desktop */}
-      <div className="container mx-auto flex justify-between items-center">
-        <Logo />
-        <LinksPages mode={mode} setMode={setMode} />
-        {/* Mobile */}
-        <div className="sm:hidden text-2xl cursor-pointer">
-          <AiOutlineMenu />
-        </div>
-      </div>
-    </nav>
+    <>
+      {isRanderModeSvg && (
+        <nav className="h-[10vh] shadow-lg flex items-center">
+          {/* Desktop */}
+          <div className="container mx-auto flex justify-between items-center">
+            <Logo />
+            <LinksPages mode={mode} setMode={setMode} />
+            {/* Mobile */}
+            <div className="sm:hidden text-2xl cursor-pointer">
+              <AiOutlineMenu />
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
 
--- hooks/useThemeSwitcher.js
+++ hooks/useThemeSwitcher.js
@@ -3,50 +3,54 @@ import React, { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
   const preferDarkQuery = `(prefer-color-schema: dark)`;
-  const [mode, setMode] = useState(localStorage.getItem("theme") || "");
+  if (typeof window !== "undefined") {
+    const [mode, setMode] = useState(localStorage.getItem("theme") || "");
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia(preferDarkQuery);
-    const usePref = localStorage.getItem("theme");
+    useEffect(() => {
+      const mediaQuery = window.matchMedia(preferDarkQuery);
+      const usePref = localStorage.getItem("theme");
 
-    const handleChange = () => {
-      if (usePref) {
-        let check = usePref === "dark" ? "dark" : "light";
-        setMode(check);
-        if (check === "dark") {
-          document.documentElement.classList.add("dark");
+      const handleChange = () => {
+        if (usePref) {
+          let check = usePref === "dark" ? "dark" : "light";
+          setMode(check);
+          if (check === "dark") {
+            document.documentElement.classList.add("dark");
+          } else {
+            document.documentElement.classList.remove("dark");
+          }
         } else {
-          document.documentElement.classList.remove("dark");
+          let check = mediaQuery.matches ? "dark" : "light";
+          setMode(check);
+          if (check === "dark") {
+            document.documentElement.classList.add("dark");
+          } else {
+            document.documentElement.classList.remove("dark");
+          }
         }
-      } else {
-        let check = mediaQuery.matches ? "dark" : "light";
-        setMode(check);
-        if (check === "dark") {
-          document.documentElement.classList.add("dark");
-        } else {
-          document.documentElement.classList.remove("dark");
-        }
+      };
+      handleChange();
+
+      mediaQuery.addEventListener("change", handleChange);
+
+      return () => mediaQuery.removeEventListener("change", handleChange);
+    }, []);
+
+    useEffect(() => {
+      if (mode === "dark") {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.add("dark");
       }
-    };
-    handleChange();
+      if (mode === "light") {
+        localStorage.setItem("theme", "light");
+        document.documentElement.classList.remove("dark");
+      }
+    }, [mode]);
 
-    mediaQuery.addEventListener("change", handleChange);
-
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
-
-  useEffect(() => {
-    if (mode === "dark") {
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    }
-    if (mode === "light") {
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    }
-  }, [mode]);
-
-  return [mode, setMode];
+    return { mode, setMode };
+  } else {
+    return { error: null };
+  }
 };
 
 export default useThemeSwitcher;
